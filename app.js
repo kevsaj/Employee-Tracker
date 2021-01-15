6d015ab202cee0b0fa2437ef57cb02b2bc1de6b5
Kevin Sajan <kevsaj@gmail.com>
2021-01-15 00:12:01 -0500

    database creation and display

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 const chalk = require('chalk');
+const log = console.log;
+const error = chalk.bold.red;
 
-console.log(chalk.yellow('Yo Wat-Up!'));
+log(chalk.blue.bgYellow.bold('Employee Manager'));
 
 const connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
   user: 'root',
-  password: '',
+  password: 'bcvc2745',
   database: 'employeesdb'
 });
+
+connection.connect((err) => {
+  if (err) throw err;
+  showEmployees();
+});
+
+const showEmployees = () => {
+    const query = connection.query(
+        `SELECT * FROM employeesdb.employees`,
+        function (err, res) {
+            if (err) throw err;
+            console.table(res);
+        }
+    )
+}
--- seeds.sql
+++ seeds.sql
@@ -1,2 +1,31 @@
 DROP DATABASE IF EXISTS employeesdb;
 CREATE DATABASE employeesdb;
+USE employeesdb;
+CREATE TABLE EMPLOYEES( id INT NOT NULL auto_increment,
+                        first_name VARCHAR ( 30 ) NOT NULL,
+                        last_name VARCHAR ( 30 ) NOT NULL,
+                        title VARCHAR ( 30 ) NOT NULL,
+                        department VARCHAR ( 80 ) NOT NULL,
+                        salary DECIMAL NOT NULL,
+                        manager VARCHAR ( 80 ) NOT NULL,
+                        PRIMARY KEY ( id ) );
+CREATE TABLE DEPARTMENT( id INT NOT NULL auto_increment,
+                        first_name VARCHAR ( 30 ) NOT NULL,
+                        last_name VARCHAR ( 30 ) NOT NULL,
+                        PRIMARY KEY ( id ) );
+CREATE TABLE _ROLE( id INT NOT NULL auto_increment,
+                        title VARCHAR ( 30 ) NOT NULL,
+                        department_id INT NOT NULL,
+                        salary DECIMAL NOT NULL,
+                        manager VARCHAR ( 80 ) NOT NULL,
+                        PRIMARY KEY ( id ) );              
+INSERT INTO EMPLOYEES(first_name, last_name, title, department, salary, manager)
+VALUES ('Mama', 'Yo', 'CEO', 'Everything', 1000000, 'The Almighty');
+INSERT INTO EMPLOYEES(first_name, last_name, title, department, salary, manager)
+VALUES ( 'Indiana', 'Jones', 'CTO', 'Everything', 999999, 'Mama, Yo');
+INSERT INTO EMPLOYEES(first_name, last_name, title, department, salary, manager)
+VALUES ( 'Edna', 'Mode', 'Designer', 'UI/UX', 999998, 'Mama, Yo' );
+INSERT INTO EMPLOYEES(first_name, last_name, title, department, salary, manager)
+VALUES ( 'Marty', 'McFly', 'Chicken', 'Sales', 10, 'Mama, Yo' );
+INSERT INTO EMPLOYEES(first_name, last_name, title, department, salary, manager)
+VALUES ( 'Clark', 'Kent', 'Journalist', 'Production', 50, 'Lois Lane' );
